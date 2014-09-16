c24d70349e4e62ef2db8d8a51d607b4107e1498c
Ian Mulvany <ian@mulvany.net>
2014-09-16 09:02:01 +0100

    added DB boilerplate

--- hello.py
+++ hello.py
@@ -1,6 +1,12 @@
+import sys
 import os
 from flask import Flask
 import requests
+
+from flask.ext.sqlalchemy import SQLAlchemy
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+db = SQLAlchemy(app)
 
 app = Flask(__name__)
 
@@ -9,4 +15,4 @@ def hello():
     r = requests.get('http://httpbin.org/status/418')
     times = int(os.environ.get('TIMES',3))
     print r.text
-    return r.text * times 
+    return r.text * times
