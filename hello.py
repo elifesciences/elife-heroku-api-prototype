519b4059e8fbe0dbc4eb72943cd4afaee58e5975
Ian Mulvany <ian@mulvany.net>
2014-09-16 09:19:52 +0100

    uri endpoints for putting data into postgres

--- hello.py
+++ hello.py
@@ -9,9 +9,37 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    email = db.Column(db.String(120))
+
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+
+    def __repr__(self):
+        return '<Name %r>' % self.name
+
 @app.route('/')
 def hello():
     r = requests.get('http://httpbin.org/status/418')
     times = int(os.environ.get('TIMES',3))
     print r.text
     return r.text * times
+
+@app.route('/putperson')
+def putperson():
+    user = User('John Doe', 'john.doe@example.com')
+    db.session.add(user)
+    db.session.commit()
+    return "user created"
+
+@app.route('/showperson')
+def showpeople():
+    all_users = User.query.all()
+    user_names = []
+    for user in all_users:
+        user_names.append(user.name)
+    output = "\n".join(user_names)
+    return output
