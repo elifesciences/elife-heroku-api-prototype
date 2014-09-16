61e0e794904129ab2fd7b63859eec361dcd7ee09
Ian Mulvany <ian@mulvany.net>
2014-09-16 09:03:44 +0100

    added DB boilerplate

--- hello.py
+++ hello.py
@@ -5,10 +5,9 @@ import requests
 
 from flask.ext.sqlalchemy import SQLAlchemy
 
+app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
-
-app = Flask(__name__)
 
 @app.route('/')
 def hello():
