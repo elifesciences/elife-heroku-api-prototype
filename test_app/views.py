1df1c03dec83b9900f6c12e139e4a314cc4fa769
Ian Mulvany <ian@mulvany.net>
2014-09-21 00:21:35 +0100

    flask rest endpoints added

--- test_app/views.py
+++ test_app/views.py
@@ -3,69 +3,10 @@ from test_app import db
 import requests
 from test_app import models
 import random
+from flask.ext.restful import reqparse, abort, Api, Resource
+from flask import jsonify
 
-def gen_random_term():
-	terms = ["gene", "hox", "peterson", "alpha"]
-	this_term = random.choice(terms)
-	term_types = ["organisim", "author keyword", "keyword"]
-	this_term_type = random.choice(term_types)
-	term = models.Term(term=this_term, term_type=this_term_type)
-	return term
-
-def select_random_term():
-	all_terms = models.Term.query.all()
-	print all_terms
-	if all_terms:
-		random_term = random.choice(all_terms)
-		return random_term
-	else:
-		return None
-
-def prep_terms():
-	t = gen_random_term()
-	u = gen_random_term()
-	s = select_random_term()
-	inject_terms = [t,u]
-	if s and s not in inject_terms:
-		inject_terms.append(s)
-	return inject_terms
-
-def gen_random_author():
-	names = ["bob", "joe", "sue", "ann"]
-	this_name = random.choice(names)
-	author = models.Author(name=this_name)
-	return author
-
-def select_random_author():
-	authors = models.Author.query.all()
-	if authors:
-		random_author = random.choice(authors)
-		return random_author
-	else:
-		return None
-
-def prep_authors():
-	a = gen_random_author()
-	b = gen_random_author()
-	s = select_random_author()
-	inject_authors = [a,b]
-	if s and s not in inject_authors:
-		inject_authors.append(s)
-	return inject_authors
-
-@app.route('/putarticle')
-def putarticle():
-
-	inject_terms = prep_terms()
-	inject_authors = prep_authors()
-	print inject_terms
-	print inject_authors
-
-	art = models.Article(title='This is the title', doi='this is the DOI',
-				pub_date="2014-08-10", terms=inject_terms, authors=inject_authors)
-	db.session.add(art)
-	db.session.commit()
-	return "article created"
+api = Api(app)
 
 @app.route('/showarticles')
 def showarticles():
@@ -90,3 +31,153 @@ def showarticles():
 
 	output = "<br/>".join(article_details)
 	return output
+
+def unpack_authors_from_article(article):
+	authors = article.authors
+	author_names = []
+	for author in authors:
+		author_names.append(author.name)
+	return author_names
+
+def unpack_terms_from_article(article, this_term_type):
+	terms = article.terms
+	print terms
+	return_terms = []
+	for term in terms:
+		if term.term_type == this_term_type:
+			return_terms.append(term.term)
+	return return_terms
+
+def get_article_representation(uid):
+	this_article = models.Article.query.filter_by(doi=uid).first()
+	author_names = unpack_authors_from_article(this_article)
+	keywords = unpack_terms_from_article(this_article, "keywords")
+	subjects = unpack_terms_from_article(this_article, "subjects")
+	organisms = unpack_terms_from_article(this_article, "organisms")
+	rep = [
+			{"_id": this_article.doi,
+			"published_at": this_article.pub_date,
+			"name": this_article.title,
+			"journal": "eLife",
+			"article_type": "Research-Article",
+			"authors": author_names,
+			"keywords": keywords,
+			"subjects": subjects,
+			"organisms": organisms,
+			"url": "documents/" + uid
+			}
+	]
+	return rep
+
+def generate_lens_index(uids):
+	all_reps = []
+	for uid in uids:
+		try:
+			article_rep = get_article_representation(uid)
+			all_reps.append(article_rep)
+		except:
+			continue
+	return all_reps
+
+def get_article_uids():
+	articles = models.Article.query.all()
+	dois = []
+	for article in articles: dois.append(article.doi)
+	return dois
+
+def get_existing_or_create_new_article(uid):
+	this_article = models.Article.query.filter_by(doi=uid).first()
+	if this_article == None:
+		this_article = models.Article(doi=uid)
+	return this_article
+
+def get_existing_or_create_new_author(author_name):
+	this_author = models.Author.query.filter_by(name=author_name).first()
+	if this_author == None:
+		this_author = models.Author(name=author_name)
+	return this_author
+
+def get_existing_or_create_new_authors(author_names):
+	authors = []
+	for author_name in author_names:
+		this_author = get_existing_or_create_new_author(author_name)
+		authors.append(this_author)
+	return authors
+
+def get_existing_or_create_new_term(term_description):
+	print term_description
+	term_name, term_type = term_description.split(":")
+	print term_name, term_type
+	this_term = models.Term.query.filter_by(term=term_name).first()  # watch out, this means we can't have differt types with different names
+	if this_term == None:
+		this_term = models.Term()
+		this_term.term = term_name
+		this_term.term_type = term_type
+	return this_term
+
+def get_existing_or_create_new_terms(term_descriptions):
+	terms = []
+	for term_description in term_descriptions:
+		this_term = get_existing_or_create_new_term(term_description)
+		terms.append(this_term)
+	return terms
+
+def create_update_research_article_on_uid(uid, update_info):
+	this_article = get_existing_or_create_new_article(uid)
+	print update_info
+	keys = update_info.keys()
+	# if we were updating simple attributes then the following might work
+	# could try something like a.__dict__.update(d)
+	# however we need to create new objects for terms and authors, based off of our DB classes
+	# so this approach won't work
+	for key in keys:
+		print key, update_info[key]
+		if key == "title" : this_article.title = update_info[key]
+		if key == "pub_date" : this_article.pub_date = update_info[key]
+		if key == "authors":
+			author_names = update_info[key]
+			author_objects = get_existing_or_create_new_authors(author_names)
+			this_article.authors = author_objects
+		if key == "terms":
+			term_descriptions = update_info[key]
+			term_objects = get_existing_or_create_new_terms(term_descriptions)
+			this_article.terms = term_objects
+	db.session.add(this_article)
+	db.session.commit()
+	return True
+
+class LensIndex(Resource):
+	def get(self):
+		uids = get_article_uids()
+		lens_index = generate_lens_index(uids)
+		return jsonify(objects=lens_index)
+
+class ArticleList(Resource):
+	def get(self):
+		uids = get_article_uids()
+		return jsonify(dois=uids)
+
+class Article(Resource):
+	def get(self, uid):
+		article_info = get_article_representation(uid)
+		return jsonify(article=article_info)  # jsonify moved to decorator to allow decorator to inspect return value of function
+
+	def put(self, uid):
+		attributes = optional_parser.parse_args()
+		update_info = {}
+		if attributes["title"]: update_info["title"] = attributes["title"]
+		if attributes["pub_date"]: update_info["pub_date"] = attributes["pub_date"]
+		if attributes["terms"]: update_info["terms"] = attributes["terms"]
+		if attributes["authors"]: update_info["authors"] = attributes["authors"]
+		create_update_research_article_on_uid(uid, update_info)  # create the article in the DB
+		return uid, 201
+
+optional_parser = reqparse.RequestParser() #  arguments that are optional
+optional_parser.add_argument('title', type=str, help="a title for the article")
+optional_parser.add_argument('pub_date', type=str, help="a date for the article")
+optional_parser.add_argument('terms', type=str, action='append', help="types of terms for the article")
+optional_parser.add_argument('authors', type=str, action='append', help="authors of the article")
+
+api.add_resource(ArticleList, '/articles')
+api.add_resource(Article, '/articles/uid/<string:uid>')
+api.add_resource(LensIndex, '/lens')
